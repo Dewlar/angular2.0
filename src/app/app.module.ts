c0d2c28861027eb44a9aba1af56b0d97ad16bb70
Andrei Markouski <insawm@gmail.com>
2024-08-29 10:24:21 +0300

    chore: rename home page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CoreInterceptor } from './core/interceptors/core.interceptor';
-import { HomeComponent } from './core/pages/home/home.component';
+import { HomePageComponent } from './core/pages/home-page/home-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TableComponent } from './core/components/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    HomePageComponent,
     TableComponent
   ],
   imports: [
